6d1c5a3c94db2f9d343f985b7c58570023e999ba
xiaohua001 <690662794@qq.com>
2021-01-11 11:09:31 +0800

    111

--- lib/routes/Routes.dart
+++ lib/routes/Routes.dart
@@ -19,7 +19,7 @@ final routes = {
 //固定写法 
 // ignore: top_level_function_literal_block
 var onGenerateRoute = (RouteSettings settings) {
-  // 统一处理钩子
+  // 统一处理
   final String name = settings.name;
   final Function pageContentBuilder = routes[name];
   if (pageContentBuilder != null) {
