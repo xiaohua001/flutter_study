9a22e7b8f225ecd29032c0f8c2e7e9ee9cd72b7d
xiaohua001 <690662794@qq.com>
2021-01-18 14:21:48 +0800

    image_piker

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,99 @@
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:device_info/device_info.dart';
 
-void main() => runApp(MyApp());
+// import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DevicePage(),
+      title: 'Image Picker Demo',
+      home: ImagePickerPage(),
     );
   }
 }
 
-class DevicePage extends StatefulWidget {
-  DevicePage({Key key}) : super(key: key);
-
-  _DevicePageState createState() => _DevicePageState();
+class ImagePickerPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ImagePickerState();
+  }
 }
 
-class _DevicePageState extends State<DevicePage> {
-  @override
-  void initState() {
-    super.initState();
-    this._getDevice();
-  }
-  _getDevice() async{
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-    print('设备号 ${androidInfo.androidId}');
-  }
+class _ImagePickerState extends State<ImagePickerPage> {
+
+  // final ImagePicker picker = new ImagePicker();
+  File _userImage;
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Device演示"),
-      ),
-      body: Text("打印到控制台"),
-    );
+        appBar: AppBar(
+          title: Text("ImagePicker"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: _takePhoto,
+                child: Text("拍照"),
+              ),
+              RaisedButton(
+                onPressed: _openGallery,
+                child: Text("选择图库照片"),
+              ),
+              _buildImage()
+            ],
+          ),
+        ));
   }
+
+  Future _takePhoto() async {
+    // final cameraImages = await picker.getImage(source: ImageSource.camera);
+    // if (mounted) {
+    //   setState(() {
+    //     //拍摄照片不为空
+    //     if (cameraImages != null) {
+    //       _userImage = File(cameraImages.path);
+    //       print('你选择的路径是：${_userImage.toString()}');
+    //       //图片为空
+    //     } else {
+    //       print('没有照片可以选择');
+    //     }
+    //   });
+    // }
+  }
+  Future _openGallery() async {
+    // final pickerImages = await picker.getImage(source: ImageSource.gallery);
+    // if(mounted){
+    //   setState(() {
+    //     if(pickerImages != null){
+    //       _userImage = File(pickerImages.path);
+    //       print('你选择的本地路径是：${_userImage.toString()}');
+    //     }else{
+    //       print('没有照片可以选择');
+    //     }
+    //   });
+    // }
+  }
+
+
+
+  //定义一个组件显示图片
+  Widget _buildImage() {
+    if (this._userImage == null) {
+      return Text("请选择图片...");
+    }
+    return Image.file(this._userImage);
+  }
+
+
 }
--- lib/native/photo.dart
+++ lib/native/photo.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-import 'package:image_picker/image_picker.dart';
+// import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,8 +27,8 @@ class ImagePickerPage extends StatefulWidget {
 
 class _ImagePickerState extends State<ImagePickerPage> {
 
-final ImagePicker picker = new ImagePicker();
-File _userImage;
+  // final ImagePicker picker = new ImagePicker();
+  File _userImage;
 
 
 
@@ -56,37 +56,36 @@ File _userImage;
         ));
   }
 
-Future _takePhoto() async {
-  final cameraImages = await picker.getImage(source: ImageSource.camera);
-  if (mounted) {
-    setState(() {
-      //拍摄照片不为空
-      if (cameraImages != null) {
-        _userImage = File(cameraImages.path);
-        print('你选择的路径是：${_userImage.toString()}');
-        //图片为空
-      } else {
-        print('没有照片可以选择');
-      }
-    });
+  Future _takePhoto() async {
+    // final cameraImages = await picker.getImage(source: ImageSource.camera);
+    // if (mounted) {
+    //   setState(() {
+    //     //拍摄照片不为空
+    //     if (cameraImages != null) {
+    //       _userImage = File(cameraImages.path);
+    //       print('你选择的路径是：${_userImage.toString()}');
+    //       //图片为空
+    //     } else {
+    //       print('没有照片可以选择');
+    //     }
+    //   });
+    // }
   }
-}
-Future _openGallery() async {
-  //选择相册
-  final pickerImages = await picker.getImage(source: ImageSource.gallery);
-  if(mounted){
-    setState(() {
-      if(pickerImages != null){
-        _userImage = File(pickerImages.path);
-        print('你选择的本地路径是：${_userImage.toString()}');
-      }else{
-        print('没有照片可以选择');
-      }
-    });
+  Future _openGallery() async {
+    // final pickerImages = await picker.getImage(source: ImageSource.gallery);
+    // if(mounted){
+    //   setState(() {
+    //     if(pickerImages != null){
+    //       _userImage = File(pickerImages.path);
+    //       print('你选择的本地路径是：${_userImage.toString()}');
+    //     }else{
+    //       print('没有照片可以选择');
+    //     }
+    //   });
+    // }
   }
-}
 
-  
+
 
   //定义一个组件显示图片
   Widget _buildImage() {
